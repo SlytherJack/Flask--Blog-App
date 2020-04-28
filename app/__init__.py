7a56cdd5952df2e2d824ed02afe9ab5cc75981dc
PradeepVigEgregore <pradeep@egregorelabs.com>
2020-04-29 00:48:48 +0530

    Features: 1. Completed Login and Registration pages.

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, flash, request, redirect
+from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'fe69ced23ce91aec103f3c0f252c4b81'
 
 posts = [
     {
@@ -12,12 +16,33 @@ posts = [
 
 @app.route('/')
 @app.route('/home')
-def hello_world():
+def home():
     return render_template('home.html', posts=posts)
 
 @app.route('/about')
 def about():
     return '<h1>About Page</h1>'
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+
+    if request.method == 'POST' and form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'pradeepovig@gmail.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 # If this file is not imported in some other module
 if __name__ == '__main__':
