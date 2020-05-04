e1213a1b28bf3aeda38b980d85aa138b0898b640
PradeepVigEgregore <pradeep@egregorelabs.com>
2020-05-05 00:38:37 +0530

    Added reset token logic to User model

--- app/__init__.py
+++ app/__init__.py
@@ -1,49 +1,17 @@
-from flask import Flask, render_template, url_for, flash, request, redirect
-from forms import RegistrationForm, LoginForm
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = 'fe69ced23ce91aec103f3c0f252c4b81'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
-posts = [
-    {
-        'title': 'My first blog post',
-        'author': 'Pradeep Vig',
-        'date_posted': '28 Apr 2020',
-        'content': 'This is a sample post'
-    }
-]
 
-@app.route('/')
-@app.route('/home')
-def home():
-    return render_template('home.html', posts=posts)
-
-@app.route('/about')
-def about():
-    return '<h1>About Page</h1>'
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    form = RegistrationForm()
-
-    if request.method == 'POST' and form.validate_on_submit():
-        flash(f'Account created for {form.username.data}!', 'success')
-        return redirect(url_for('home'))
-
-    return render_template('register.html', title='Register', form=form)
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        if form.email.data == 'pradeepovig@gmail.com' and form.password.data == 'password':
-            flash('You have been logged in!', 'success')
-            return redirect(url_for('home'))
-        else:
-            flash('Login Unsuccessful. Please check username and password', 'danger')
-    return render_template('login.html', title='Login', form=form)
-
-# If this file is not imported in some other module
-if __name__ == '__main__':
-    app.run(debug=True)
+from app import views
--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
-from app import db, login_manager
+from app import db, login_manager, app
 from datetime import datetime
 from flask_login import UserMixin
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
 
 @login_manager.user_loader
@@ -16,6 +17,24 @@ class User(db.Model, UserMixin):
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
     post = db.relationship('Post', backref='author', lazy=True)
+
+    def get_reset_token(self, expires_sec=1800):
+        s = Serializer(app.config['SECRET_KEY'], expires_sec)
+        return s.dumps({'user_id': self.id}).decode('utf-8')
+
+
+    @staticmethod
+    def verify_reset_token(token):
+        s = Serializer(app.config['SECRET_KEY'])
+
+        try:
+            # Load user id from serialized object
+            user_id = s.loads(token)['user_id']
+        except:
+            return None
+
+        return User.query.get(user_id)
+
 
     # Overridden: This will print out the official representation of our User
     def __repr__(self):
